3c74bf4d1093471be28ac88797a8eed277108cd0
Hieu X. Leu <hieuleuxuan@gmail.com>
2023-08-25 15:26:18 +1000

    Initial Commit

---
+++ FollowTask/FollowTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollowTaskApp.swift
+//  FollowTask
+//
+//  Created by Hieu Xuan Leu on 25/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct FollowTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
